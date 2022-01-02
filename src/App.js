d09079b52d636051e5e1dd9f1a9c508cf8d2c2c1
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-01-02 18:15:43 +0530

    ADDED PROVIDER AND REDUCER

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/header/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ const App = () => {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 };
 
--- src/components/Layout/HeaderButton/HeaderCartButton.jsx
+++ src/components/Layout/HeaderButton/HeaderCartButton.jsx
@@ -1,14 +1,22 @@
 import classes from "./headerCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../../store/CartContext";
 
 const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
